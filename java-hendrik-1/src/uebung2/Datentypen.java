90a26fcead14a7a8b97bd0b8858a0608caf0b687
David <david.bergstreisser@gmail.com>
2020-09-29 21:21:51 +0200

    Datentypen

---
+++ java-hendrik-1/src/uebung2/Datentypen.java
@@ -0,0 +1,10 @@
+package uebung2;
+
+public class Datentypen {
+    public static void main(String[] args) {
+        int zahl = 70;
+        float gewicht;
+        int age;
+        age = 42;
+    }
+}
