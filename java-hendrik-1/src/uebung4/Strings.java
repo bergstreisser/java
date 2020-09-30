5540e9104be50e7a273661bb79d5ba77d7ee3325
David <david.bergstreisser@gmail.com>
2020-09-30 21:00:56 +0200

    Modul 2

---
+++ java-hendrik-1/src/uebung4/Strings.java
@@ -0,0 +1,13 @@
+package uebung4;
+
+public class Strings {
+    public static void main(String[] args) {
+        String sentence1;
+        String var = "Heute scheint die Sonne!";
+
+//        sentence1 = var;
+//        System.out.println(sentence1);
+
+          System.out.println(sentence1 = var);
+    }
+}
