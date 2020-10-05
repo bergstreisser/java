9a1418c10fb409f4a8ca3d2e252dc645453368a4
David <david.bergstreisser@gmail.com>
2020-10-05 11:32:09 +0200

    Modul 6

--- java-hendrik-1/src/modul6/uebung11/Konsolenspiel.java
+++ java-hendrik-1/src/modul6/uebung11/Konsolenspiel.java
@@ -5,29 +5,44 @@ import java.util.Scanner;
 public class Konsolenspiel {
     public static void main(String[] args) {
         int zahl, antwort;
-        boolean ende = false;
+        int untergrenze = 1;
+        int obergrenze = 1000;
+        int frageZahl = (obergrenze +  untergrenze) / 2;
+        int versuche = 1;
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Bitte gib eine Zahl zwischen 1 und 1000 ein: ");
+        System.out.println("Bitte gib eine Ganzzahl zwischen "+untergrenze+" und "+obergrenze+" ein: ");
         zahl = scanner.nextInt();
 
         do {
-            System.out.println("Ist Deine Zahl = 500? 1 - Ja");
-            System.out.println("Ist Deine Zahl < 500? 2 - Ja");
-            System.out.println("Ist Deine Zahl > 500? 3 - Ja");
+            System.out.println("Ist Deine Zahl = " + frageZahl + "? 1 - Ja");
+            System.out.println("Ist Deine Zahl < " + frageZahl + "? 2 - Ja");
+            System.out.println("Ist Deine Zahl > " + frageZahl + "? 3 - Ja");
             antwort = scanner.nextInt();
-            ende = getAntwort(antwort);
 
+            if(antwort == 1) {
+                System.out.println("Deine Zahl ist: " +frageZahl);
+                System.out.println("Du hast " + versuche + " Versuche gebraucht!");
+            } else if(antwort == 2) {
+                obergrenze = frageZahl;
+                frageZahl = getFragezahl(obergrenze, untergrenze);
+                //int div = obergrenze - untergrenze;
+                //frageZahl = obergrenze - (div / 2);
+                versuche++;
+            } else if(antwort == 3) {
+                untergrenze = frageZahl;
+                frageZahl = getFragezahl(obergrenze, untergrenze);
+                //int div = obergrenze - untergrenze;
+                //frageZahl = untergrenze + (div / 2);
+                versuche++;
+            } else {
+                System.out.println("Bitte nur 1, 2, oder 3 eingeben! ");
+            }
 
-        } while (ende == false);
-
+        } while (antwort != 1);
     }
 
-    public static boolean getAntwort(int zahl) {
-        if(zahl == 1) {
-            return true;
-        } else {
-            return false;
-        }
+    public static int getFragezahl(int obergrenze, int untergrenze) {
+        return (obergrenze +  untergrenze) / 2;
     }
 }
