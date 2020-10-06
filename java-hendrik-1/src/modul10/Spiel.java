eab19ad2b725eb15a965793f436f4f04fa801232
David <david.bergstreisser@gmail.com>
2020-10-06 11:29:53 +0200

    Modul 10 - Spielfeld

---
+++ java-hendrik-1/src/modul10/Spiel.java
@@ -0,0 +1,54 @@
+package modul10;
+
+import java.util.Scanner;
+
+public class Spiel {
+    public static void main(String[] args) {
+
+        String aktion = "v";
+        boolean spiel = true;
+
+        Spielfeld spielfeld = new Spielfeld();
+        Spieler spieler = new Spieler();
+        spielfeld.zeichneFeld(spieler, aktion);
+
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            System.out.println();
+            System.out.println("Aktion: g = gehen, l = links drehen, r = rechts drehen, v = vorwärtz drehen, q = beenden");
+            aktion = scanner.nextLine();
+
+            switch (aktion) {
+                case "g": {
+                    if (
+                            spieler.getyPosition() < spielfeld.getGRENZE_OBEN() ||
+                            spieler.getyPosition() > spielfeld.getGRENZE_UNTEN() ||
+                            spieler.getxPosition() < spielfeld.getGRENZE_LINKS() ||
+                            spieler.getxPosition() > spielfeld.getGRENZE_RECHTS()
+                    ) {
+                        System.out.println("gehen nicht möglich!");
+                    } else {
+                        spielfeld.zeichneFeld(spieler, aktion);
+                    }
+                    break;
+                }
+                case "l":
+                    spielfeld.zeichneFeld(spieler, aktion);
+                    break;
+                case "r":
+                    spielfeld.zeichneFeld(spieler, aktion);
+                    break;
+                case "v":
+                    spielfeld.zeichneFeld(spieler, aktion);
+                    break;
+                case "q":
+                    System.out.println("Spiel beendet!");
+                    return;
+                default:
+                    System.out.println("Falsche Eingabe!");
+            }
+
+        } while (spiel);
+    }
+}
---
+++ java-hendrik-1/src/modul10/Spieler.java
@@ -0,0 +1,63 @@
+package modul10;
+
+public class Spieler {
+
+    private int xPosition = 10;
+    private int yPosition = 9;
+    private String richtung = "^";
+
+    public int getxPosition() {
+        return xPosition;
+    }
+
+    public void setxPosition(int xPosition) {
+        this.xPosition = xPosition;
+    }
+
+    public int getyPosition() {
+        return yPosition;
+    }
+
+    public void setyPosition(int yPosition) {
+        this.yPosition = yPosition;
+    }
+
+    public String getRichtung() {
+        return richtung;
+    }
+
+    public void setRichtung(String richtung) {
+        this.richtung = richtung;
+    }
+
+    public void gehen() {
+        for(int j = 0; j < 20; j++) {
+            if(j == 0) {
+                System.out.println();
+                System.out.print("#");
+            } else if(j == xPosition) {
+                System.out.print(richtung);
+            } else if (j == 19) {
+                System.out.print("#");
+            } else {
+                System.out.print(" ");
+            }
+        }
+    }
+
+    public void spielerDrehen(int pos) {
+        for(int j = 0; j < 20; j++) {
+            if(j == 0) {
+                System.out.println();
+                System.out.print("#");
+            } else if(j == pos) {
+                System.out.print(richtung);
+            } else if (j == 19) {
+                System.out.print("#");
+            } else {
+                System.out.print(" ");
+            }
+        }
+    }
+
+}
