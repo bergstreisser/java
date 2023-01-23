8b74ded7fcfaa0c73c1efb8d41c206ffa495926e
JoNNy <JoNNy@10.6.7.157>
2023-01-23 11:46:07 +0100

    [taschenrechnerMain Class]: Einlesen und Switch Case

--- Taschenrechner/taschenrechner/taschenrechnerMain.java
+++ Taschenrechner/taschenrechner/taschenrechnerMain.java
@@ -1,10 +1,51 @@
 package taschenrechner;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class taschenrechnerMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		boolean state = true;
+		String eingabe = "0";
+		int eingabeFromString = 0;
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		while(state) {
+			System.out.println("Rechenoperation?");
+			System.out.println("<0> Ende <1> Plus");
+			System.out.println("<2> Minus <3> Mal");
+			System.out.println("<4> Geteilt");
+			
+			try {
+				eingabe = reader.readLine();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			eingabeFromString = Integer.parseInt(eingabe);
+			
+			switch(eingabeFromString) {
+			case 0:
+				
+				break;
+			case 1:
+				
+				break;
+			case 2:
+				
+				break;
+			case 3:
+				
+				break;
+			case 4:
+				
+				break;
+			
+			}//end switch Case
+				
+		}//end while
 
 	}
-
 }
