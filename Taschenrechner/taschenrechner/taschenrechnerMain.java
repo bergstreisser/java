8f35f1cf88ed3ebf72225ce9e6b3a61ba48317d3
JoNNy <JoNNy@10.6.7.157>
2023-01-23 12:36:26 +0100

    [taschenrechnerMain Class]: Minus

--- Taschenrechner/taschenrechner/taschenrechnerMain.java
+++ Taschenrechner/taschenrechner/taschenrechnerMain.java
@@ -8,6 +8,7 @@ public class taschenrechnerMain {
 
 	public static void main(String[] args) throws IOException {
 		boolean state = true;
+		int zahl1, zahl2 = 0;
 		String eingabe = "0";
 		int eingabeFromString = 0;
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -27,7 +28,6 @@ public class taschenrechnerMain {
 				state = false;
 				break;
 			case 1:
-				int zahl1, zahl2 = 0;
 				System.out.println("Erste Zahl?");
 				eingabe = reader.readLine();
 				zahl1 = Integer.parseInt(eingabe);
@@ -40,6 +40,15 @@ public class taschenrechnerMain {
 				
 				break;
 			case 2:
+				System.out.println("Erste Zahl?");
+				eingabe = reader.readLine();
+				zahl1 = Integer.parseInt(eingabe);
+				
+				System.out.println("Zweite Zahl?");
+				eingabe = reader.readLine();
+				zahl2 = Integer.parseInt(eingabe);
+				
+				System.out.println("Das Ergebnis ist: " + (zahl1 - zahl2));
 				
 				break;
 			case 3:
