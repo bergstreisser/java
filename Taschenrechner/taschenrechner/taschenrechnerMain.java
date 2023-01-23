334d476f23f5b8079da2460576bc2724e48c540f
JoNNy <JoNNy@10.6.7.157>
2023-01-23 12:18:01 +0100

    [taschenrechnerMain Class]: Plusrechnen

--- Taschenrechner/taschenrechner/taschenrechnerMain.java
+++ Taschenrechner/taschenrechner/taschenrechnerMain.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 
 public class taschenrechnerMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		boolean state = true;
 		String eingabe = "0";
 		int eingabeFromString = 0;
@@ -17,20 +17,26 @@ public class taschenrechnerMain {
 			System.out.println("<2> Minus <3> Mal");
 			System.out.println("<4> Geteilt");
 			
-			try {
-				eingabe = reader.readLine();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
+
+			eingabe = reader.readLine();
 			eingabeFromString = Integer.parseInt(eingabe);
 			
 			switch(eingabeFromString) {
 			case 0:
-				
+				System.out.println("Prozedur wird beendet");
+				state = false;
 				break;
 			case 1:
+				int zahl1, zahl2 = 0;
+				System.out.println("Erste Zahl?");
+				eingabe = reader.readLine();
+				zahl1 = Integer.parseInt(eingabe);
+				
+				System.out.println("Zweite Zahl?");
+				eingabe = reader.readLine();
+				zahl2 = Integer.parseInt(eingabe);
+				
+				System.out.println("Das Ergebnis ist: " + (zahl1 + zahl2));
 				
 				break;
 			case 2:
