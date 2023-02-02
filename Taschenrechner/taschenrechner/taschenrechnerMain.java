09e267f949fbbed75b8e412e57eae2930f96608b
fdai7551 <jona-lukas.zahn@informatik.hs-fulda.de>
2023-02-02 13:02:28 +0100

    Subtrahieren von 4 Zahlen hinzugefügt

--- Taschenrechner/taschenrechner/taschenrechnerMain.java
+++ Taschenrechner/taschenrechner/taschenrechnerMain.java
@@ -82,7 +82,7 @@ public class taschenrechnerMain {
 				break;
 				
 			case 2:
-				System.out.println("Sollen 2 oder 3 zahlen subtrahiert werden?");
+				System.out.println("Sollen 2, 3 oder 4 zahlen subtrahiert werden?");
 				eingabe = reader.readLine();
 				anzahlElemente = Integer.parseInt(eingabe);
 				
@@ -111,6 +111,26 @@ public class taschenrechnerMain {
 					zahl3 = Integer.parseInt(eingabe);
 					
 					System.out.println("Das Ergebnis ist: " + (zahl1 - zahl2 - zahl3));
+				}
+				
+				if(anzahlElemente==4) {
+					System.out.println("Erste Zahl?");
+					eingabe = reader.readLine();
+					zahl1 = Integer.parseInt(eingabe);
+					
+					System.out.println("Zweite Zahl?");
+					eingabe = reader.readLine();
+					zahl2 = Integer.parseInt(eingabe);
+					
+					System.out.println("Dritte Zahl?");
+					eingabe = reader.readLine();
+					zahl3 = Integer.parseInt(eingabe);
+					
+					System.out.println("Vierte Zahl?");
+					eingabe = reader.readLine();
+					zahl4 = Integer.parseInt(eingabe);
+					
+					System.out.println("Das Ergebnis ist: " + (zahl1 - zahl2 - zahl3 - zahl4));
 				}
 				break;
 			case 3:
